f9db44e23977ca206f6783e889b575b97a2b1bbb
Rob Sanchez <rob@robsanchez.com>
2015-10-31 09:07:42 -0500

    EE3 compatibility

---
+++ system/expressionengine/third_party/http_header/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'author'      => 'Rob Sanchez',
+    'author_url'  => 'https://github.com/rsanchez',
+    'name'        => 'HTTP Header',
+    'description' => 'Set the HTTP Headers for your template.',
+    'version'     => '1.1.0',
+    'namespace'   => '\\',
+);
--- system/expressionengine/third_party/http_header/pi.http_header.php
+++ system/expressionengine/third_party/http_header/pi.http_header.php
@@ -2,61 +2,11 @@
 
 $plugin_info = array(
 	'pi_name' => 'HTTP Header',
-	'pi_version' => '1.0.9',
+	'pi_version' => '1.1.0',
 	'pi_author' => 'Rob Sanchez',
 	'pi_author_url' => 'https://github.com/rsanchez',
 	'pi_description' => 'Set the HTTP Headers for your template.',
-	'pi_usage' => '# HTTP Header
-
-Set the HTTP Headers for your template.
-
-## Parameters
-
-* status - set an HTTP Status code
-* location - set a location for redirection
-* content_type - set a Content-Type header
-* charset - set a charset in the Content-Type header
-* content_disposition - set a Content-Disposition header (ex: attachment) with a filename
-* content_language - set a Content-Language header
-* cache_seconds - set to a non-zero number to set caching headers; set to 0 to force no-cache
-* terminate - set to "yes" to prevent any other output from the template
-* vary - set a Vary header
-* access_control_allow_origin - set a Access-Control-Allow-Origin header
-
-## Examples
-
-Do a 301 redirect
-
-	{exp:http_header status="301" location="{path=site/something}" terminate="yes"}
-
-Set a 404 Status header
-
-	{exp:http_header status="404"}
-
-Set the Content-Type header to application/json
-
-	{exp:http_header content_type="application/json"}
-
-Set Content-Disposition to force the download
-
-	{exp:http_header content_disposition="attachment" filename="myfile.xml"}
-
-Set the Pragma, Cache-control, and Expires headers to set a 5 minute (300 second) cache
-
-	{exp:http_header cache_seconds="300"}
-
-Set the Content-Language header to "en"
-
-	{exp:http_header content_language="en"}
-
-Set the Vary header to User-Agent
-
-    {exp:http_header vary="User-Agent"}
-
-Set the Access-Control-Allow-Origin header to allow all
-
-    {exp:http_header access_control_allow_origin="*"}
-',
+	'pi_usage' => 'See https://github.com/rsanchez/http_header',
 
 );
 
